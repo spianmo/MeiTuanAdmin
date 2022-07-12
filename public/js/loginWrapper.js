f990e257a06a0d7e3d8847987e8a45b6e0c1e79c
Finger <finger@spianmo.com>
2022-07-13 01:15:21 +0800

    feat: getCookie

--- public/js/loginWrapper.js
+++ public/js/loginWrapper.js
@@ -1,25 +1,22 @@
 const ipcRenderer = require('electron').ipcRenderer
 
-function getPostList () {
-    const postElems = document.querySelectorAll('.entry-list>li.item')
-    const list = []
-    postElems.forEach(item => {
-        const linkElem = item.querySelector('.title-row a') || {}
-        list.push({
-            title: linkElem.innerText || '',
-            link: linkElem.href || ''
-        })
-    })
-    return list
-}
+const $ = (id) => document.getElementById(id)
+const $$ = (className) => document.getElementsByClassName(className)
 
-function main () {
+function main() {
     console.log("fuvk")
     // 延迟时间等dom渲染
     setTimeout(() => {
-        const list = getPostList()
-        ipcRenderer.sendToHost('data', JSON.stringify(list))
+        $('login').setAttribute("value", "wmlxxm2381471");
+        $('login').dispatchEvent(new Event('input', {"bubbles": true, "cancelable": true}));
+        $('password').setAttribute("value", "d057506");
+        $('password').dispatchEvent(new Event('input', {"bubbles": true, "cancelable": true}));
+        $$('login__submit')[0].click()
+
     }, 100)
+    ipcRenderer.on('getCookie', () => {
+        ipcRenderer.sendToHost('receiveCookie', document.cookie)
+    })
 }
 
 main()
