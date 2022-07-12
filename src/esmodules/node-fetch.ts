6f5c217ba65252f98527e397111635e9c319f7d6
Finger <finger@spianmo.com>
2022-07-13 02:49:49 +0800

    feat: 美团外卖模拟登陆后窗口的管理

--- src/esmodules/node-fetch.ts
+++ src/esmodules/node-fetch.ts
@@ -2,6 +2,5 @@ import fetch from 'node-fetch'
 
 (async () => {
   const body = await (await fetch('https://baidu.com/')).text()
-  console.log("fuck!!!!!!!!")
-  console.log(body)
+  //console.log(body)
 })()
